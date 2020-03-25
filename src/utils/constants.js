8e2035dbc52a7bcf2b6caeda1e29631deba2dd9f
Maalai <maalai1989@gmail.com>
2020-03-25 11:52:18 +0530

    Maalai: Updated API url to get all campaigns

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const adminLoginUrl = '/api/csr/admin/login';
-export const getAllCampaignsUrl = '/api/csr/campaigns/live';
+export const getAllCampaignsUrl = '/api/csr/campaigns?live=true';
 export const getACampaignDetails = '/api/csr/campaigns/';
 export const getImageUrl = '/api/csr/images/';
 export const fetchLocationURL = '/api/csr/location';
