f51460e827576bc2f008cba88790e760f07ff236
Maalai <maalai1989@gmail.com>
2020-04-11 12:46:58 +0530

    Maalai: KML file url changed

--- src/components/HotZonesComponent.js
+++ src/components/HotZonesComponent.js
@@ -1,11 +1,16 @@
 import { GoogleMap, KmlLayer, withGoogleMap, withScriptjs } from 'react-google-maps';
 import React from 'react';
 
+const kmlFileUrl = () => {
+    return window.location.host.includes('localhost') ? 'https://coviddev.gccservice.in/hotzones.kml' : window.location.host + '/hotzones.kml';
+};
+
 export const HotZonesComponent = withScriptjs(
     withGoogleMap(props => {
+        console.log('host is: ', kmlFileUrl());
         return (
             <GoogleMap defaultZoom={10} defaultCenter={{ lat: 13.0827, lng: 80.2707 }}>
-                <KmlLayer url="/hotzones.kml" options={{ preserveViewport: true }} zIndex={100}/>
+                <KmlLayer url={kmlFileUrl()} options={{ preserveViewport: true }} zIndex={100}/>
             </GoogleMap>
         );
     }),
