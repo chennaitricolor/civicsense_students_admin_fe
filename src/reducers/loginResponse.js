d84c38195139ae29cd8f2af620830f7dd879d656
Nandha Kumar <nandha.kumar@mrcooper.com>
2020-07-05 21:53:35 +0530

    TECH: Nandha: Added is gcc flag to false

--- src/reducers/loginResponse.js
+++ src/reducers/loginResponse.js
@@ -10,13 +10,19 @@ const defaultState = {
 const loginResponse = (state = defaultState, action) => {
   switch (action.type) {
     case actions.INITIATE_LOGIN:
-      return Object.assign({}, state, { inProgress: true });
+      return Object.assign({}, state, { loginMessage: '', inProgress: true });
     case actions.LOGIN_SUCCESS:
       localStorage.setItem('persona', action.payload.persona);
       localStorage.setItem('region', action.payload.region);
       return Object.assign({}, state, { ...action.payload, inProgress: false });
     case actions.LOGIN_FAILURE:
-      return Object.assign({}, state, { success: false, persona: '', region: ', inProgress: false,' });
+      return Object.assign({}, state, {
+        loginMessage: action.response,
+        success: false,
+        persona: '',
+        region: '',
+        inProgress: false,
+      });
     default:
       return state;
   }
