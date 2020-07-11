5bb001d610d1c6cdc7362162b9375ab9ecd9dd7d
Nandha Kumar <nandha.kumar@mrcooper.com>
2020-07-11 21:26:38 +0530

    TECH: Nandha: Added missing files for Logout

---
+++ src/sagas/logoutSaga.js
@@ -0,0 +1,29 @@
+import { put, call } from 'redux-saga/effects';
+import logoutActions from '../actions/logout';
+import routeToPathAction from '../actions/routeToPath';
+import { callFetchApi } from '../services/api';
+import { adminLogoutUrl } from '../utils/constants';
+
+export default function* logoutSaga() {
+  try {
+    const response = yield call(callFetchApi, adminLogoutUrl, {}, 'DELETE');
+    if (response.data !== undefined && response.data.success) {
+      yield put({
+        type: logoutActions.LOGOUT_SUCCESS,
+        payload: response.data,
+      });
+      yield put({
+        type: routeToPathAction.ROUTE_TO_PATH,
+        payload: { path: '/' },
+      });
+    } else {
+      yield put({
+        type: logoutActions.LOGOUT_FAILURE,
+      });
+    }
+  } catch (e) {
+    yield put({
+      type: logoutActions.LOGOUT_FAILURE,
+    });
+  }
+}
